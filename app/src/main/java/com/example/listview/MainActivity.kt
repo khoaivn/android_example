6563e31bd95eee721ff22090affae6f6bd888f37
phamgi <trungdunght2003@gmail.com>
2023-04-21 16:53:39 +0700

    connect firebase

--- app/src/main/java/com/example/listview/MainActivity.kt
+++ app/src/main/java/com/example/listview/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.listview
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.ContentValues
+import android.content.ContentValues.TAG
 import android.database.Cursor
 import android.os.Bundle
 import android.util.Log
@@ -13,6 +14,12 @@ import android.widget.ImageView
 import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,62 +29,78 @@ class MainActivity : AppCompatActivity() {
         install_datebase()
         get_data()
 
-        val listView = findViewById<ListView>(R.id.list_view)
-
-        val language = arrayOf<String>("C","C++","Java",".Net","Kotlin","Ruby","Rails","Python","Java Script","Php","Ajax","Perl","Hadoop")
-        val description = arrayOf<String>(
-            "C programming is considered as the base for other programming languages",
-            "C++ is an object-oriented programming language.",
-            "Java is a programming language and a platform.",
-            ".NET is a framework which is used to develop software applications.",
-            "Kotlin is a open-source programming language, used to develop Android apps and much more.",
-            "Ruby is an open-source and fully object-oriented programming language.",
-            "Ruby on Rails is a server-side web application development framework written in Ruby language.",
-            "Python is interpreted scripting  and object-oriented programming language.",
-            "JavaScript is an object-based scripting language.",
-            "PHP is an interpreted language, i.e., there is no need for compilation.",
-            "AJAX allows you to send and receive data asynchronously without reloading the web page.",
-            "Perl is a cross-platform environment used to create network and server-side applications.",
-            "Hadoop is an open source framework from Apache written in Java."
-        )
-
-        val imageId = arrayOf<Int>(
-            R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_background
-        )
-
-//        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
-        listView.adapter =  MyListAdapter(this, language, description, imageId)
+//        val listView = findViewById<ListView>(R.id.list_view)
+//
+//        val language = arrayOf<String>("C","C++","Java",".Net","Kotlin","Ruby","Rails","Python","Java Script","Php","Ajax","Perl","Hadoop")
+//        val description = arrayOf<String>(
+//            "C programming is considered as the base for other programming languages",
+//            "C++ is an object-oriented programming language.",
+//            "Java is a programming language and a platform.",
+//            ".NET is a framework which is used to develop software applications.",
+//            "Kotlin is a open-source programming language, used to develop Android apps and much more.",
+//            "Ruby is an open-source and fully object-oriented programming language.",
+//            "Ruby on Rails is a server-side web application development framework written in Ruby language.",
+//            "Python is interpreted scripting  and object-oriented programming language.",
+//            "JavaScript is an object-based scripting language.",
+//            "PHP is an interpreted language, i.e., there is no need for compilation.",
+//            "AJAX allows you to send and receive data asynchronously without reloading the web page.",
+//            "Perl is a cross-platform environment used to create network and server-side applications.",
+//            "Hadoop is an open source framework from Apache written in Java."
+//        )
+//
+//        val imageId = arrayOf<Int>(
+//            R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_background
+//        )
+//
+////        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
+//        listView.adapter =  MyListAdapter(this, language, description, imageId)
     }
     fun install_datebase(){
-        val dbHelper = MyDatabaseHelper(this)
-        val db = dbHelper.writableDatabase
+//        val dbHelper = MyDatabaseHelper(this)
+//        val db = dbHelper.writableDatabase
+//
+//        val values = ContentValues()
+//        values.put("name", "Pham Trung Dung")
+//        values.put("email", "dung.phamtrung@phenikaa-uni.edu.vn")
+//
+//        db.insert("users", null, values)
 
-        val values = ContentValues()
-        values.put("name", "John Doe")
-        values.put("email", "john.doe@example.com")
+        val database = Firebase.database
+        val myRef = database.getReference("/")
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue<String>()
+                Log.d("TAG", "Value is: $value")
+            }
 
-        db.insert("users", null, values)
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("TAG", "Failed to read value.", error.toException())
+            }
+        })
 
     }
     fun get_data(){
-        val dbHelper = MyDatabaseHelper(this)
-        val db = dbHelper.readableDatabase
-
-        val cursor: Cursor = db.rawQuery("SELECT * FROM users", null)
-
-        while (cursor.moveToNext()) {
-            val id: Int = cursor.getInt(cursor.getColumnIndex("id"))
-            val name: String = cursor.getString(cursor.getColumnIndex("name"))
-            val email: String = cursor.getString(cursor.getColumnIndex("email"))
-            Log.d("MyApp", "id: $id, name: $name, email: $email")
-        }
-
-        cursor.close()
-        db.close()
+//        val dbHelper = MyDatabaseHelper(this)
+//        val db = dbHelper.readableDatabase
+//
+//        val cursor: Cursor = db.rawQuery("SELECT * FROM users", null)
+//
+//        while (cursor.moveToNext()) {
+//            val id: Int = cursor.getInt(cursor.getColumnIndex("id"))
+//            val name: String = cursor.getString(cursor.getColumnIndex("name"))
+//            val email: String = cursor.getString(cursor.getColumnIndex("email"))
+//            Log.d("MyApp", "id: $id, name: $name, email: $email")
+//        }
+//
+//        cursor.close()
+//        db.close()
 
     }
 }
@@ -89,7 +112,6 @@ class MyListAdapter(private val context: Activity, private val title: Array<Stri
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
         val inflater = context.layoutInflater
         val rowView = inflater.inflate(R.layout.item_listview, null, true)
-
 
 
         val titleText = rowView.findViewById(R.id.title) as TextView
