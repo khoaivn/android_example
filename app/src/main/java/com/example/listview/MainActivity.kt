a7bb1a094503669ca47d357daba195f048d23f5b
phamgi <trungdunght2003@gmail.com>
2023-04-28 08:40:53 +0700

    done get data firebase

--- app/src/main/java/com/example/listview/MainActivity.kt
+++ app/src/main/java/com/example/listview/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.listview
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
@@ -22,155 +24,75 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        install_database()
-//        get_data()
-
-        val listView = findViewById<ListView>(R.id.list_view)
-
-        val language = arrayOf<String>("C","C++","Java",".Net","Kotlin","Ruby","Rails","Python","Java Script","Php","Ajax","Perl","Hadoop")
-        val description = arrayOf<String>(
-            "C programming is considered as the base for other programming languages",
-            "C++ is an object-oriented programming language.",
-            "Java is a programming language and a platform.",
-            ".NET is a framework which is used to develop software applications.",
-            "Kotlin is a open-source programming language, used to develop Android apps and much more.",
-            "Ruby is an open-source and fully object-oriented programming language.",
-            "Ruby on Rails is a server-side web application development framework written in Ruby language.",
-            "Python is interpreted scripting  and object-oriented programming language.",
-            "JavaScript is an object-based scripting language.",
-            "PHP is an interpreted language, i.e., there is no need for compilation.",
-            "AJAX allows you to send and receive data asynchronously without reloading the web page.",
-            "Perl is a cross-platform environment used to create network and server-side applications.",
-            "Hadoop is an open source framework from Apache written in Java."
-        )
+        install_database(this)
 
         val imageId = arrayOf<Int>(
-            R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
             R.drawable.ic_launcher_background
         )
 
-//        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
-        listView.adapter =  MyListAdapter(this, language, description, imageId)
     }
-    private fun install_database(){
-//        val dbHelper = MyDatabaseHelper(this)
-//        val db = dbHelper.writableDatabase
-//
-//        val values = ContentValues()
-//        values.put("name", "Pham Trung Dung")
-//        values.put("email", "dung.phamtrung@phenikaa-uni.edu.vn")
-//
-//        db.insert("users", null, values)
+
+    private fun install_database(context: Activity) {
 
         val students_list = mutableListOf<Student>()
         val database = Firebase.database
         val myRef = database.getReference("/")
         myRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-//                val value = dataSnapshot.getValue<String>()
+
                 val students_data = dataSnapshot.child("students")
-
-//                println(students_data.toString())
                 val childIterator = students_data.children.iterator()
-
-                // Iterate through the child nodes and do something with each one
                 while (childIterator.hasNext()) {
-
                     val childSnapshot = childIterator.next()
-                    val childKey = childSnapshot.key
-//                    val childValue = childSnapshot.child("code").getValue(String::class.java)
                     val name = childSnapshot.child("name").getValue(String::class.java)
                     val code = childSnapshot.child("code").getValue(String::class.java)
                     students_list.add(Student(name, code))
-                    println("Child key: $childKey, Child value: $code")
                 }
-
+                println(students_list.get(0).name)
+                val listView = findViewById<ListView>(R.id.list_view)
+                listView.adapter = MyAdapter(context, students_list)
             }
 
             override fun onCancelled(error: DatabaseError) {
-                // Failed to read value
                 Log.w("TAG", "Failed to read value.", error.toException())
             }
         })
 
     }
-    fun get_data(){
-//        val dbHelper = MyDatabaseHelper(this)
-//        val db = dbHelper.readableDatabase
-//
-//        val cursor: Cursor = db.rawQuery("SELECT * FROM users", null)
-//
-//        while (cursor.moveToNext()) {
-//            val id: Int = cursor.getInt(cursor.getColumnIndex("id"))
-//            val name: String = cursor.getString(cursor.getColumnIndex("name"))
-//            val email: String = cursor.getString(cursor.getColumnIndex("email"))
-//            Log.d("MyApp", "id: $id, name: $name, email: $email")
-//        }
-//
-//        cursor.close()
-//        db.close()
+}
+class MyAdapter(context: Context, private val students_list: List<Student>) :
+    ArrayAdapter<Student>(context, 0, students_list) {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var view = convertView
 
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.item_listview, parent, false)
+        }
+
+        val item = students_list[position]
+
+        val tv_title = view?.findViewById<TextView>(R.id.title)
+        val tv_description = view?.findViewById<TextView>(R.id.title)
+        tv_title!!.text = item.name
+        tv_description!!.text = item.code
+
+        return view!!
+    }
+    override fun getCount(): Int {
+        return students_list.size
     }
 }
 
-class MyListAdapter(private val context: Activity, private val students_list: List<Student>)
-    : ArrayAdapter<String>(context, R.layout.item_listview, title) {
 
-    @SuppressLint("ViewHolder")
-    override fun getView(position: Int, view: View?, parent: ViewGroup): View {
-        val inflater = context.layoutInflater
-        val rowView = inflater.inflate(R.layout.item_listview, null, true)
-
-
-        val titleText = rowView.findViewById(R.id.title) as TextView
-        val imageView = rowView.findViewById(R.id.icon) as ImageView
-        val subtitleText = rowView.findViewById(R.id.description) as TextView
-
-        titleText.text = title[position]
-        imageView.setImageResource(imgid[position])
-        subtitleText.text = description[position]
-
-        return rowView
-    }
-}
-//class MyListAdapter(private val context: Context, private val items: List<String>) : BaseAdapter() {
-//
-//    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-//        val view: View
-//        val viewHolder: ViewHolder
-//
-//        if (convertView == null) {
-//            view = LayoutInflater.from(context).inflate(R.layout.item_listview, parent, false)
-//            viewHolder = ViewHolder(view)
-//            view.tag = viewHolder
-//        } else {
-//            view = convertView
-//            viewHolder = convertView.tag as ViewHolder
-//        }
-//
-//        viewHolder.itemTextView.text = items[position]
-//
-//        return view
-//    }
-//
-//    override fun getItem(position: Int): Any {
-//        return items[position]
-//    }
-//
-//    override fun getItemId(position: Int): Long {
-//        return position.toLong()
-//    }
-//
-//    override fun getCount(): Int {
-//        return items.size
-//    }
-//
-//    private class ViewHolder(view: View) {
-//        val itemTextView: TextView = view.findViewById(R.id.text1)
-//    }
-//}
